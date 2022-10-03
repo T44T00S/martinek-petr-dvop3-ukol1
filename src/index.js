845f76c4a9e78aed8e02d2703e1fe72b38c98ca2
Petr Martinek <petrklic2005@gmail.com>
2022-10-03 16:11:28 +0200

    adding pozn. for code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+//Array
 var myArray = ["Lodash", "React", "Next", "Strapi", "Axios", "Typescript"]
 myArray.forEach(element => {
     console.log(element)
 });
+
+//Reversed array
 console.log(myArray.reverse())
+
+//Sorted array
 console.log(myArray.sort())
 
 var car = {
@@ -11,11 +16,12 @@ var car = {
     Year: "2021"
 }
 
+//Object
 console.log(car.Brand)
 const paramBrand = console.log(car.Brand)
 const paramModel = console.log(car.Model)
 const paramYear = console.log(car.Year)
 
+//Random number
 var randomNo = Math.floor(Math.random() * 555)
-
 console.log('Náhodné číslo je ' + randomNo)
